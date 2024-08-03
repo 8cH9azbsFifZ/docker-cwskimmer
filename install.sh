acd07c62d003ed02a8a73f959491ff49f10452ba
Gerolf Ziegenhain <gerolf.ziegenhain@gmail.com>
2024-08-03 10:36:49 +0200

    intel

--- install.sh
+++ install.sh
@@ -5,5 +5,8 @@
 # Install VCRedist for CWSL
 /usr/local/bin/winetricks -q vcrun2010 
 
+# Install VCRedist for CWSL_DIGI
+/usr/local/bin/winetricks -q vcrun2022
+
 cd /install/SkimmerSrv_1.6
 wine Setup.exe /SILENT
